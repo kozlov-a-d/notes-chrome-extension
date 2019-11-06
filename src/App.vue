b984514780aba829f06bee4635612c5d3d6692fa
Andy <andreydmitrievich666@gmail.com>
2019-11-06 15:17:12 +0300

    update

--- src/App.vue
+++ src/App.vue
@@ -4,22 +4,83 @@
     <!-- <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <p>test</p>
-    {{ todos }}
+    <div class="notes">
+      <ul>
+        <li v-for="note in notes" v-bind:key="note.title">
+          <strong>{{ note.title }}</strong> 
+          <span>{{ note.text }}</span> 
+          <span>{{ note.createdAt }}</span>
+        </li>
+      </ul>
+
+      <form class="form" @submit="addNote">
+        <div class="form__item">
+          <input v-model="newNote.title" type="text" placeholder="title">
+        </div>
+        <div class="form__item">
+          <textarea v-model="newNote.text" placeholder="text"></textarea>
+        </div>
+        <button type="submit">add</button>
+        <div class="form__errors">
+          <ul>
+            <li v-for="error in newNote.errors" v-bind:key="error">{{error}}</li>
+          </ul>
+        </div>
+      </form>
+      
+    </div>
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
-import { db } from './db';
+import { db, Timestamp } from './db';
 
 export default {
   name: 'app',
-  data: () => ({ todos: [] }),
+  data: () => ({ 
+    notes: [],
+    newNote: {
+      title: '',
+      text: '',
+      createdAt: '',
+      errors: []
+    }
+  }),
   firestore: {
-    todos: db.collection('todos'),
+    notes: db.collection('todos'),
   },
   components: {
     // HelloWorld
+  },
+  created: function () {
+    
+  },
+  methods: {
+    addNote: function (e) {
+      e.preventDefault();
+
+       this.newNote.errors= [];
+
+      if ( this.newNote.title.length == 0) {
+        this.newNote.errors.push(`field 'title' can't been empty`);
+      }
+
+      if ( this.newNote.title.length == 0) {
+        this.newNote.errors.push(`field 'text' can't been empty`);
+      }
+
+      if (this.newNote.errors.length == 0) {
+        // this.notes.push()
+        db.collection('todos').add({
+          title: this.newNote.title,
+          text: this.newNote.text,
+          createdAt: Timestamp.fromDate(new Date()),
+        })
+      }
+
+      return false;
+    }
   }
 }
 </script>
--- src/db.js
+++ src/db.js
@@ -2,3 +2,5 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 export const db = firebase.initializeApp({ projectId: 'beontop-chrome-extention', }).firestore();
+
+export const Timestamp = firebase.firestore.Timestamp;
