e7d2acd6055d9c8591d568e5aa1689e903b466cd
Andy <andreydmitrievich666@gmail.com>
2019-11-12 17:27:29 +0300

    update

--- dist/background.js
+++ dist/background.js
@@ -3,13 +3,39 @@
  * крутится всегда и один для всего браузера, умеет только слать сообщения на разные слои и слушать их.
  */
 
+ // TODO(DEVELOPER): Change the values below using values from the initialization snippet: Firebase Console > Overview > Add Firebase to your web app.
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAtBeQBW1JujViibEohFXg4tBfU7RZfEls",
+    authDomain: "beontop-chrome-extention.firebaseapp.com",
+    databaseURL: "https://beontop-chrome-extention.firebaseio.com",
+    projectId: "beontop-chrome-extention",
+    storageBucket: "beontop-chrome-extention.appspot.com",
+    messagingSenderId: "135859589645",
+    appId: "1:135859589645:web:544b9af1ebb422ed1ffb44"
+};
+
+firebase.initializeApp(config);
+// console.log(firebase.app().name);  // "[DEFAULT]"
+
+var defaultStorage = firebase.storage();
+var defaultFirestore = firebase.firestore();
+let notes = defaultFirestore.collection('todos');
+var myUserId = firebase.auth().currentUser.uid;
+
+// console.log(notes.orderBy("title").limit(3));
+console.log(myUserId);
+
+
+
 function checkCurrentUrl(tabId, tabUrl) {
     var pattern=/(.+:\/\/)?([^\/]+)(\/.*)*/i;
     var arr=pattern.exec(tabUrl);
     var value = arr[2].replace('www.', '');
     // Устанавливаем значение бейджу. Идентификатор указаывается в качестве ограничителя.
     // Т.е для вкладок подходящих под условие значение 3, а для всех остальных 0
-    chrome.browserAction.setBadgeText({text: value, tabId: tabId});
+    // chrome.browserAction.setBadgeText({text: value, tabId: tabId});
+    console.log({text: value, tabId: tabId});
 } 
 
 function changeBadgeTextOnActivated(tabInfo) {
